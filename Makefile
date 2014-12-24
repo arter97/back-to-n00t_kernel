arter97/android_kernel_asus_flo

Makefiles: compiler option overhaul #2
 - Remove all cutting-edge flags

   Linux kernel should be stable what-so-ever.
   I've found numerous articles on the web that
   GCC optimization flags can actually hurt "very much".

   Let's go back to the conservative step; disable the followings :
   -Ofast -fno-tree-vectorize -fmodulo-sched -fmodulo-sched-allow-regmoves -fno-inline-functions -funroll-loops

 - Go back to soft-float

   @GeeckoDev have tipped me, that the Linux kernel actually does not use
   the floating-point calculation.
   Using hard-float is non-standard and can hurt.
   So go back to soft-float.

 - Use vfpv4 instead of neon for mfpu flag

   GCC neon code generation can end up in funky behaviours.
   There are beneficial codes that explicitly defines '-mfpu=neon',
   so disable neon globally.

 - Disable PIC code generation

   Seems like Linux kernel is not being friendly with PIC code generation.
   Reference : https://android.googlesource.com/kernel/common.git/+/b2a582d102c4deb9e6a34f784e0700f642c50e8a

Signed-off-by: arter97 <qkrwngud825@gmail.com>

Makefile
@@ -380,7 +380,7 @@ KBUILD_CFLAGS   := -Wall -Wundef -Wstrict-prototypes -Wno-trigraphs \
 		   -std=gnu89
 
 # arter97's optimizations
-KBUILD_CFLAGS	+= -s -pipe -Ofast -mcpu=cortex-a15 -mtune=cortex-a15 -mfloat-abi=hard -mfpu=vfpv4 -fno-tree-vectorize -fmodulo-sched -fmodulo-sched-allow-regmoves -fno-inline-functions -funroll-loops
+KBUILD_CFLAGS	+= -s -pipe -fno-pic -O2 -mcpu=cortex-a15 -mtune=cortex-a15 -mfloat-abi=softfp -mfpu=vfpv4
 # -Wno-unused
 KBUILD_CFLAGS	+= -Wno-unused
 # L1/L2 cache size parameters by @JustArchi

arter97/back-to-n00t_kernel

   -Ofast -fno-tree-vectorize -fmodulo-sched -fmodulo-sched-allow-regmoves -fno-inline-functions
 - Use vfpv3 instead of neon for mfpu flag

Makefile
@@ -375,7 +375,7 @@ KBUILD_CFLAGS   := -Wall -Wundef -Wstrict-prototypes -Wno-trigraphs \
 		   -std=gnu89
 
 # arter97's optimizations
-KBUILD_CFLAGS	+= -s -pipe -Ofast -mcpu=cortex-a9 -mtune=cortex-a9 -march=armv7-a -mfloat-abi=hard -mfpu=vfpv3 -fno-tree-vectorize -fmodulo-sched -fmodulo-sched-allow-regmoves -fno-inline-functions
+KBUILD_CFLAGS	+= -s -pipe -fno-pic -O2 -mcpu=cortex-a9 -mtune=cortex-a9 -mfloat-abi=softfp -mfpu=vfpv3
 # -Wno-unused
 KBUILD_CFLAGS	+= -Wno-unused
 # L1/L2 cache size parameters by @JustArchi